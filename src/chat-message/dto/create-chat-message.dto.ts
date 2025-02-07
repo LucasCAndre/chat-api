381d893b8e089c214a5b648ca295ee2932e7f59c
Lucas Andre <lucas.andre@ereadvisory.com>
2025-02-06 22:48:14 -0300

    feat(dto): implements create and update chat message dtos with validator

--- src/chat-message/dto/create-chat-message.dto.ts
+++ src/chat-message/dto/create-chat-message.dto.ts
@@ -1,1 +1,16 @@
-export class CreateChatMessageDto {}
+import { IsUUID, IsString, IsOptional } from 'class-validator';
+
+export class CreateChatMessageDto {
+  @IsUUID()
+  chat_id: string;
+
+  @IsUUID()
+  user_id: string;
+
+  @IsString()
+  message: string;
+
+  @IsUUID()
+  @IsOptional()
+  reply_to?: string;
+}
--- src/chat-message/dto/update-chat-message.dto.ts
+++ src/chat-message/dto/update-chat-message.dto.ts
@@ -1,1 +1,4 @@
-export class UpdateChatMessageDto {}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateChatMessageDto } from './create-chat-message.dto';
+
+export class UpdateChatMessageDto extends PartialType(CreateChatMessageDto) {}
