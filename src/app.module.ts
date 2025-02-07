45cffd482e2dfef6f062a5a1494d0ef27f171dce
Lucas Andre <lucas.andre@ereadvisory.com>
2025-02-06 23:31:42 -0300

    feat(database): connect to database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,34 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { UsersModule } from './users/users.module';
 import { ChatMessageModule } from './chat-message/chat-message.module';
-import { ChatModule } from './chat/chat.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ChatModule, ChatMessageModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ChatModule,
+    ChatMessageModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/chat-message/chat-message.module.ts
+++ src/chat-message/chat-message.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatMessageService } from './chat-message.service';
 import { ChatMessageController } from './chat-message.controller';
+import { ChatMessage } from './entities/chat-message.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ChatMessage])], // Importando o TypeOrmModule para o repositório da entidade
   controllers: [ChatMessageController],
-  providers: [ChatMessageService],
+  providers: [ChatMessageService], // Certifique-se de que o serviço está listado nos providers
 })
 export class ChatMessageModule {}
